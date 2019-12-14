9b74b6e9b99a8fea2aa44186878de03b08b84d44
dingyu <ourstoryzj@163.com>
2019-12-14 13:51:10 +0800

    sss

--- js/import.js
+++ js/import.js
@@ -4,5 +4,28 @@ script_temp.setAttribute('type','text/javascript');
 script_temp.setAttribute('src','http://raw.githack.com/ourstoryzj/chrome_plug-in/master/js/common.js');
 document.getElementsByTagName('head')[0].appendChild(script_temp);
 
- 
-//addScript('http://raw.githack.com/ourstoryzj/chrome_plug-in/master/js/showbox.js');
+script_temp =document.createElement('script');
+script_temp.setAttribute('type','text/javascript');
+script_temp.setAttribute('src','http://raw.githack.com/ourstoryzj/chrome_plug-in/master/js/showbox.js');
+document.getElementsByTagName('head')[0].appendChild(script_temp);
+
+script_temp =document.createElement('script');
+script_temp.setAttribute('type','text/javascript');
+script_temp.setAttribute('src','http://raw.githack.com/ourstoryzj/chrome_plug-in/master/js/jquery.js');
+document.getElementsByTagName('head')[0].appendChild(script_temp);
+
+script_temp =document.createElement('script');
+script_temp.setAttribute('type','text/javascript');
+script_temp.setAttribute('src','http://raw.githack.com/ourstoryzj/chrome_plug-in/master/js/taobao_login.js');
+document.getElementsByTagName('head')[0].appendChild(script_temp);
+
+script_temp =document.createElement('script');
+script_temp.setAttribute('type','text/javascript');
+script_temp.setAttribute('src','http://raw.githack.com/ourstoryzj/chrome_plug-in/master/js/taobao.js');
+document.getElementsByTagName('head')[0].appendChild(script_temp);
+
+script_temp =document.createElement('script');
+script_temp.setAttribute('type','text/javascript');
+script_temp.setAttribute('src','http://raw.githack.com/ourstoryzj/chrome_plug-in/master/js/gittest.js');
+document.getElementsByTagName('head')[0].appendChild(script_temp);
+
